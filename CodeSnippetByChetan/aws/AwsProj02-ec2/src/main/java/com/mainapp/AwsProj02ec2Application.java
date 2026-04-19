9362c3873cee1b840c06267c1d69ed2d1603c471
SPS <123806275+spspu@users.noreply.github.com>
2026-04-19 13:00:46 +0530

    commit

---
+++ CodeSnippetByChetan/aws/AwsProj02-ec2/src/main/java/com/mainapp/AwsProj02ec2Application.java
@@ -0,0 +1,13 @@
+package com.mainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProj02ec2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProj02ec2Application.class, args);
+	}
+
+}
---
+++ CodeSnippetByChetan/aws/AwsProj02-ec2/src/test/java/com/mainapp/AwsProj02ec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mainapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProj02ec2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
