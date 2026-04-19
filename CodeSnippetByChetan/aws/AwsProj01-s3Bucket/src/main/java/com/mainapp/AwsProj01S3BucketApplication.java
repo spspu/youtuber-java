81a2bd863bd3d7e5d8d828f91dcbd8c61e733ff2
SPS <123806275+spspu@users.noreply.github.com>
2026-04-19 12:54:01 +0530

    commit

---
+++ CodeSnippetByChetan/aws/AwsProj01-s3Bucket/src/main/java/com/mainapp/AwsProj01S3BucketApplication.java
@@ -0,0 +1,13 @@
+package com.mainapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProj01S3BucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProj01S3BucketApplication.class, args);
+	}
+
+}
